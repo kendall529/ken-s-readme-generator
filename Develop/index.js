2d1c5fed537fd9660565924121bf10421130432b
kendall529 <kendall.smith529@gmail.com>
2023-05-18 23:01:13 -0500

    added prompt inputs

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,51 @@
 const Inquirer = require('inquirer');
 const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter the project title:',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a brief description of the project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information:',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter contribution guidelines:',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter test instructions:',
+    }
+
+];
+
+Inquirer
+    .prompt(questions)
+    .then((response) => {
+        const {title, description, installation, usage, contributing, tests} = response;
+        const readmeData = init(title, description, installation, usage, contributing, tests);
+        writeToFile('README.md', readmeData);
+    })
+    .catch((error) => {
+        console.error('Error', error);
+    });
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -10,7 +54,29 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init(title, description, installation, usage, contributing, tests) {
+    const mdData = `
+    # Title
+    ${title}
+    
+    ## Description
+    ${description}
+    
+    ## Installation
+    ${installation}
+
+    ## Usage
+    ${usage}
+
+    ## Contributing
+    ${contributing}
+
+    ## Tests
+    ${tests}
+    `;
+
+    return mdData;
+}
 
 // Function call to initialize app
 init();
