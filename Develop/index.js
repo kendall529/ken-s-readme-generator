2ddcb1c2da435dbc8f0147f12196a6aed1f6c5b6
kendall529 <kendall.smith529@gmail.com>
2023-05-18 23:17:25 -0500

    added licensing

--- Develop/index.js
+++ Develop/index.js
@@ -32,15 +32,22 @@ const questions = [
         type: 'input',
         name: 'tests',
         message: 'Enter test instructions:',
-    }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your application:',
+        choices: ['MIT', 'Apache', 'GNU', 'None'],
+    },
+
 
 ];
 
 Inquirer
     .prompt(questions)
     .then((response) => {
-        const {title, description, installation, usage, contributing, tests} = response;
-        const readmeData = init(title, description, installation, usage, contributing, tests);
+        const {title, description, installation, usage, contributing, tests, license} = response;
+        const readmeData = init(title, description, installation, usage, contributing, tests, license);
         writeToFile('README.md', readmeData);
     })
     .catch((error) => {
@@ -54,29 +61,53 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init(title, description, installation, usage, contributing, tests) {
+function init(title, description, installation, usage, contributing, tests, license) {
+
     const mdData = `
+    ${getLicenseBadge(license)}
     # Title
+
     ${title}
+
     
     ## Description
+
     ${description}
     
     ## Installation
+
     ${installation}
 
     ## Usage
+
     ${usage}
 
     ## Contributing
+
     ${contributing}
 
     ## Tests
+
     ${tests}
+
+    ## License
+
+    This application is license by ${license}. 
     `;
 
     return mdData;
 }
+
+function getLicenseBadge(license) {
+    // Map license names to their corresponding badge URLs
+    const licenseBadges = {
+        MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+        Apache: '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+        GNU: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+    };
+  
+    return licenseBadges[license] || '';
+  }
 
 // Function call to initialize app
 init();
